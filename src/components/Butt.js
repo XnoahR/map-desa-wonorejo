3d6265a2893c78546c3cd33b46791e52a3614aca
Rio Saputro <rio.ae23@gmail.com>
2024-07-13 15:21:37 +0700

    Layout, Header, Logo

---
+++ src/components/Butt.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Butt() {
+  return (
+    <p className=" w-4/5 text-justify ps-5 bg-gray-300 py-3 rounded-md mt-3 font-bold text-lg">
+
+        Tentukan tujuan Anda
+      
+    </p>
+  );
+}
---
+++ src/pages/user/index.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+// import Butt from "../components/Butt";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default () => {
+  return (
+    <>
+      <h1> Hello World </h1>
+    </>
+  );
+}
